4511372a81ce25fa6481ae151801469ac48eebd6
priyanshu0412 <pdevops04@gmail.com>
2025-06-22 22:28:27 +0530

    making otp res

--- src/components/verifyOtpComp/index.js
+++ src/components/verifyOtpComp/index.js
@@ -161,7 +161,7 @@ const VerifyOtpComp = () => {
                             className="mt-2 block w-full rounded-md border border-gray-300 bg-gray-50 px-4 py-2 text-gray-900 shadow-sm focus:ring-thirdColor focus:border-thirdColor transition-all"
                         />
                     </div>
-                    <div className="mt-4 flex justify-between space-x-2">
+                    <div className="mt-4 flex flex-wrap justify-center gap-2 sm:justify-between">
                         {otp.map((digit, index) => (
                             <input
                                 key={index}
@@ -171,10 +171,11 @@ const VerifyOtpComp = () => {
                                 onChange={(e) => handleOtpChange(e, index)}
                                 onPaste={handlePasteOtp}
                                 ref={(el) => (inputRefs.current[index] = el)}
-                                className="text-black w-12 h-12 text-center text-lg font-semibold rounded-md border border-gray-300 bg-gray-50 focus:ring-thirdColor focus:border-thirdColor"
+                                className="text-black w-10 h-10 sm:w-12 sm:h-12 text-center text-lg font-semibold rounded-md border border-gray-300 bg-gray-50 focus:ring-thirdColor focus:border-thirdColor"
                             />
                         ))}
                     </div>
+
                     {error && <p className="text-sm text-red-500">{error}</p>}
                     <div>
                         <button
