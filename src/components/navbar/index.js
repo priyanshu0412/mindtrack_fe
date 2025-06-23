259a47bd2c9a843262194aa902d6c78202ed0303
priyanshu0412 <pdevops04@gmail.com>
2025-06-23 20:09:19 +0530

    remove margin from the bottom warning

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -20,7 +20,7 @@ const Navbar = () => {
     return (
         <>
             {
-                showAlert && <div className="flex items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+                showAlert && <div className="flex items-center p-4 text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
                     <svg className="shrink-0 w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                     </svg>
